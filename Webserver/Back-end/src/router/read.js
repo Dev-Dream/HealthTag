bba3fbad415ed67cd7cd3d6c15c528cdb4cb2e3c
rasauq1122 <rasauq1122@gmail.com>
2020-06-22 17:12:58 +0900

    Refactoring : use router to separate function functions of main.js move to router file for maintenance optimization

---
+++ Webserver/Back-end/src/router/read.js
@@ -0,0 +1,47 @@
+const _express = require("express");
+const _mysql = require("mysql");
+
+const router = _express.Router();
+
+const db = _mysql.createConnection({
+    host     : "localhost",
+    user     : "root",
+    password : "mysql",
+    database : "healthtag"
+});
+db.connect();
+
+router.get("/part/:partName/:tags", (req, res) => {
+    var part = req.params.partName;
+    var tags = req.params.tags.split("@").slice(1);
+    var find_tag_query = "SELECT * FROM "+part+" JOIN diseases USING(name)";
+    
+    var i = 0;
+    if (tags.length != 0) {
+        find_tag_query = find_tag_query + " WHERE tag LIKE \'\%"+tags[i]+"\%\'";
+    }
+
+    while (i < tags.length) {
+        find_tag_query = find_tag_query + " AND tag LIKE \'\%"+tags[i]+"\%\'";
+        i = i + 1;
+    }
+    
+
+    var returnData = {list:[]};
+
+    db.query(find_tag_query, (err, find_tag) => {
+        var i = 0;
+        while (i < find_tag.length) {
+            var element = {name:"", tags:[], desc:"", url:""};
+            element.name = find_tag[i].name;
+            element.tags = find_tag[i].tag.split("#").slice(1);
+            element.desc = find_tag[i].description;
+            element.url = find_tag[i].URL;
+            returnData.list.push(element);
+            i = i + 1;
+        }
+        res.send(returnData);
+    });
+});
+
+module.exports = router;
