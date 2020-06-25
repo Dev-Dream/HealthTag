28740a867782d8c1b2069e81a067559b858104d0
ParkJunseong <rasauq1122@gmail.com>
2020-06-25 17:59:33 +0900

    Refactoring: fusion of Front-end and Back-end

--- Webserver/Back-end/main.js
+++ Webserver/Back-end/main.js
@@ -1,17 +1,18 @@
 const _express = require("express");
+const _path = require("path");
 
 const app = _express(); 
 const readRouter = require("./src/router/read"); 
 const deleteRouter = require("./src/router/delete");
 const createRouter = require("./src/router/create");
 const postRouter = require("./src/router/post");
-const pageRouter = require("./src/page/page");
+const pageRouter = require("./src/router/page");
 
 app.use("/", pageRouter);
 app.use("/", readRouter);
 app.use("/", deleteRouter);
 app.use("/", createRouter);
 app.use("/", postRouter);
-app.use(_express.static("src/page/public"));
+app.use(_express.static(_path.join(__dirname, "/../Front-end/public")));
 
 app.listen(3000);
---
+++ Webserver/Back-end/src/router/page.js
@@ -0,0 +1,48 @@
+const _express = require("express");
+const _session = require("express-session");
+const _path = require("path");
+const session_config = require("../important/session");
+
+const router = _express.Router();
+const front_path = _path.join(__dirname,"/../../../Front-end/pages");
+
+router.use(_session(session_config.session));
+
+router.get("/", (req, res) => {
+    res.sendFile(front_path+"/index.html");
+});
+
+router.get("/head", (req, res) => {
+    res.sendFile(front_path+"/head.html");
+});
+
+router.get("/neck", (req, res) => {
+    res.sendFile(front_path+"/neck.html");
+});
+
+router.get("/chest", (req, res) => {
+    res.sendFile(front_path+"/chest.html");
+});
+
+router.get("/arm", (req, res) => {
+    res.sendFile(front_path+"/arm.html");
+});
+
+router.get("/leg", (req, res) => {
+    res.sendFile(front_path+"/leg.html");
+});
+
+router.get("/stomach", (req, res) => {
+    res.sendFile(front_path+"/stomach.html");
+});
+
+router.get("/about", (req, res) => {
+    res.sendFile(front_path+"/about.html");
+});
+
+router.get("/admin", (req, res) => {
+    if (req.session.isLoggedin) res.sendFile(front_path+"/admin.html");
+    else res.sendFile(front_path+"/login.html");
+});
+
+module.exports = router;
